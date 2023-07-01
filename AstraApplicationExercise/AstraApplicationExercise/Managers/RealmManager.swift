1ad1c8fac232ae6fcfbca3e16ba6524458151943
Melvin Ballesteros <>
2023-07-02 01:28:49 +0800

    Updated - RealmManager Class has been updated and added it's functionalities

--- AstraApplicationExercise/AstraApplicationExercise/Managers/RealmManager.swift
+++ AstraApplicationExercise/AstraApplicationExercise/Managers/RealmManager.swift
@@ -6,3 +6,109 @@
 //
 
 import Foundation
+import RealmSwift
+
+class RealmManager {
+    
+    private init() {
+        let config = Realm.Configuration(schemaVersion: 1, deleteRealmIfMigrationNeeded: false)
+        Realm.Configuration.defaultConfiguration = config
+    }
+    
+    static let shared = RealmManager()
+    
+    /// Function - Select All
+    func getAll<T: Object>(_ object: T) -> Results<T> {
+        let realm = try! Realm()
+        return realm.objects(T.self)
+    }
+    
+    /// Function - Create
+    func create<T: Object>(_ object: T) {
+        let realm = try? Realm()
+        do {
+            try realm?.write {
+                realm?.add(object)
+            }
+        } catch {
+            print(error)
+        }
+    }
+    
+    /// Function - Mass Creation
+    func create<T: Object>(_ object: [T]) {
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    /// Function - Update
+    func update<T: Object>(_ object: T, with dictionary: [String: Any?]){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                for (key, value) in dictionary {
+                    object.setValue(value, forKey: key)
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    /// Function - Delete
+    func delete<T: Object>(_ object: T){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    /// Function - Mass Deletion
+    func delete<T: Object>(_ object: List<T>){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    /// Function - Single Deletion
+    func delete<T: Object>(_ object: Results<T>){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    // FORMAT DISK
+    func deleteAll(){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.deleteAll()
+            }
+        } catch{
+            print(error.localizedDescription)
+        }
+    }
+}
