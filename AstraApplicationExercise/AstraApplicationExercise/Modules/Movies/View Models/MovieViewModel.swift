feec3a9a2d3a66ab96a9924f6919df158a5e9c90
Melvin Ballesteros <>
2023-07-02 13:29:49 +0800

    Updated - Movie View Model functions has been updated

--- AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/View Models/MovieViewModel.swift
+++ AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/View Models/MovieViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 class MovieViewModel {
 
@@ -14,6 +15,7 @@ class MovieViewModel {
     
     
     let headerTitle = "Movies"
+    let searchTitle = "Search"
     
     var textArtwork: String {
         return movie?.artwork ?? ""
@@ -25,6 +27,88 @@ class MovieViewModel {
     
     var textTitle: String {
         return movie?.title ?? ""
+    }
+    
+    var textGenre: String {
+        return movie?.genre ?? ""
+    }
+    
+    var textPrice: String {
+        let formattedPrice = "$ \(movie?.price ?? 0.00)"
+        return formattedPrice
+    }
+    
+    var textLongDesc: String {
+        let appendText = "Description:\n\(movie?.longDesc ?? "")"
+        return appendText
+    }
+    
+    var textShortDesc: String {
+        return movie?.shortDesc ?? ""
+    }
+    
+    var errorMessage: String = ""
+
+    
+    func fetchMovies(success: @escaping(()-> Void), failed: @escaping(()-> Void)) {
+        ApiManager.shared.getRequest(Api.movie) { (data, error) in
+            if error == nil {
+                guard let data = data else { return }
+                self.saveMovies(data: data)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    success()
+                }
+            } else {
+                self.errorMessage = error?.localizedDescription ?? "Error loading page, please try again."
+                failed()
+            }
+        }
+    }
+    
+    func fetchMovies(completion: @escaping(() -> Void)) {
+        ApiManager.shared.getRequest(Api.movie) { (data, error) in
+            if error == nil {
+                guard let data = data else { return }
+                guard let json = try? JSON(data: data) else { return }
+                print(json)           
+                // save to cached
+            } else {
+                self.errorMessage = error?.localizedDescription ?? "Error loading page, please try again."
+                // fetch cached if any
+            }
+            
+            completion()
+        }
+    }
+    
+    func setSelectedMovie(_ movie: Movie) {
+        self.movie = movie
+    }
+    
+    func saveMovies(data: Data) {
+        /// Removed previous data before saving updated data
+        deleteAllData()
+        /// Parse data into json
+        guard let json = try? JSON(data: data) else { return }
+        let results = json["results"].array
+        let objects = results?.map( {Movie($0)}) ?? [Movie]()
+        
+        // Save to realm
+        MovieRepository.shared.create(objects)
+        // Fetch Data
+        fetchLocalData()
+    }
+    
+    func fetchLocalData() {
+        movies = MovieRepository.shared.getAll()
+    }
+    
+    func deleteAllData() {
+        MovieRepository.shared.deleteAll()
+    }
+    
+    func searchMovies(_ searchText: String) {
+        movies = MovieRepository.shared.search(searchText)
     }
     
 }
