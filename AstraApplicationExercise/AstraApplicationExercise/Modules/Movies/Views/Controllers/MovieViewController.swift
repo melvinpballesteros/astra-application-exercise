92e5d4edad4711b9093e185c93daee37ec7855e1
Melvin Ballesteros <>
2023-07-02 13:31:47 +0800

    Updated - Movie Controller has been updated to layout the UI's and functionalities

--- AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/Views/Controllers/MovieViewController.swift
+++ AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/Views/Controllers/MovieViewController.swift
@@ -16,8 +16,10 @@ class MovieViewController: BaselineViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupTargets()
         setupContext()
         setupTableView()
+        fetchMovies()
     }
     
     // MARK: - Setup    
@@ -29,29 +31,65 @@ class MovieViewController: BaselineViewController {
     
     private func setupContext() {
         headerLabel.text = viewModel.headerTitle
+        
+        let attributes = [ NSAttributedString.Key.foregroundColor: UIColor.lightGray,
+                           NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16, weight: .regular)]
+        
+        searchTextField.attributedPlaceholder = NSAttributedString(string: viewModel.searchTitle,
+                                                         attributes: attributes as [NSAttributedString.Key : Any])
     }
     
-    
+    private func setupTargets() {
+        searchTextField.addTarget(self, action: #selector(searchMovie), for: .editingChanged)
+    }
+
     
     // MARK: - Events
     
     
     // MARK: - Logic
     private func fetchMovies() {
+        /// Start Activity Indicator
+        activityIndicatorStart()
         
+        viewModel.fetchMovies { 
+            DispatchQueue.main.async {
+                self.refreshTableView()
+                self.activityIndicatorEnd()
+            }
+            
+        } failed: { 
+            DispatchQueue.main.async {
+                self.viewModel.fetchLocalData()
+                self.refreshTableView()
+                self.activityIndicatorEnd()
+            }
+        }
+    }
+    
+    private func refreshTableView() {
+        tableView.reloadData()
+    }
+    
+    @objc private func searchMovie() {
+        viewModel.searchMovies(searchTextField.text ?? "")
     }
 
 }
 
 extension MovieViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        viewModel.setSelectedMovie(viewModel.movies[indexPath.row])
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 180
     }
 }
 
 extension MovieViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return viewModel.movies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
