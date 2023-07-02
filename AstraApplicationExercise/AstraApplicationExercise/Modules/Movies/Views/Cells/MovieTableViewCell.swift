01e3a73b57073ad94060abae3aca6e20ca49d56c
Melvin Ballesteros <>
2023-07-02 13:30:52 +0800

    Updated - Movie Cell has been updated to layout the UI's

--- AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/Views/Cells/MovieTableViewCell.swift
+++ AstraApplicationExercise/AstraApplicationExercise/Modules/Movies/Views/Cells/MovieTableViewCell.swift
@@ -13,6 +13,38 @@ class MovieTableViewCell: UITableViewCell {
     static let reuseIdentifier = "MovieTableViewCell"
     
     // MARK: - Controls
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        label.textColor = .white
+        label.numberOfLines = 3
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var genreLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = AppColor.yellow
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.sizeToFit()
+        return label
+    }()
+    
+    lazy var favoriteButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "heart-white"), for: .normal)
+        button.addTarget(self, action: #selector(userTapFavorite), for: .touchUpInside)
+        return button
+    }()
+    
     lazy var thumbnail: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
@@ -20,11 +52,22 @@ class MovieTableViewCell: UITableViewCell {
         imageView.clipsToBounds = true
         return imageView
     }()
+    
+    lazy var backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bg-cell")
+        return imageView
+    }()
+    
+    lazy var contextView: UIView = {
+        let view = UIView()
+        return view
+    }()
 
     // MARK: - Initialization
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        setupAttributes()
         setupViews()
         setupConstraints()
     }
@@ -35,19 +78,79 @@ class MovieTableViewCell: UITableViewCell {
     
     
     // MARK: - Setup
+    private func setupAttributes() {
+        self.backgroundColor = AppColor.black
+        self.selectionStyle = .none
+    }
+    
     private func setupViews() {
+        contentView.setSubviewForAutoLayout(
+            backgroundImageView,
+            thumbnail,
+            favoriteButton,
+            contextView
+        )
         
+        contextView.setSubviewForAutoLayout(
+            titleLabel,
+            genreLabel,
+            priceLabel
+        )
     }
     
     private func setupConstraints() {
+        backgroundImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(25)
+            make.right.equalToSuperview().offset(-25)
+            make.top.equalToSuperview().offset(10)
+            make.bottom.equalToSuperview().offset(-10)
+        }
         
+        thumbnail.snp.makeConstraints { make in
+            make.left.equalTo(backgroundImageView).offset(16)
+            make.width.height.equalTo(80)
+            make.centerY.equalToSuperview()
+        }
+        
+        favoriteButton.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+            make.right.equalTo(backgroundImageView).offset(-10)
+            make.top.equalToSuperview().offset(13)
+        }
+        
+        contextView.snp.makeConstraints { make in
+            make.left.equalTo(thumbnail.snp.right).offset(16)
+            make.right.equalToSuperview().offset(-35)
+            make.centerY.equalToSuperview()
+            make.bottom.equalTo(priceLabel).offset(5)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(5)
+            make.right.equalToSuperview().offset(-16)
+            make.top.equalTo(5)
+            
+        }
+        
+        genreLabel.snp.makeConstraints { make in
+            make.left.equalTo(5)
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.right.equalToSuperview().offset(-16)
+        }
+        
+        priceLabel.snp.makeConstraints { make in
+            make.left.equalTo(5)
+            make.top.equalTo(genreLabel.snp.bottom).offset(5)
+            make.bottom.equalToSuperview().offset(-5)
+        }
     }
     
     func configure(_ viewModel: MovieViewModel) {
-        
+        titleLabel.text = viewModel.textTitle
+        genreLabel.text = viewModel.textGenre
+        priceLabel.text = viewModel.textPrice
         
         fetchImage(viewModel.textArtwork)
-        
     }
     
     
@@ -65,6 +168,12 @@ class MovieTableViewCell: UITableViewCell {
         .transition(.fade(0.3))
         .cacheOriginalImage()
         .set(to: thumbnail)
+    }
+    
+    
+    // MARK: - Events
+    @objc private func userTapFavorite() {
+         // tapFavorite!()
     }
 
 }
