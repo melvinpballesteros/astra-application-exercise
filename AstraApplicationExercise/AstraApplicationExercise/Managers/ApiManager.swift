829c8dd782559e1ba573920919387733826f3d8f
Melvin Ballesteros <>
2023-07-02 13:28:31 +0800

    Updated - APIManager Class has been updated

--- AstraApplicationExercise/AstraApplicationExercise/Managers/ApiManager.swift
+++ AstraApplicationExercise/AstraApplicationExercise/Managers/ApiManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+class ApiManager {
+    
+    static let shared = ApiManager()
+    private init() {}
+    
+    func getRequest(_ urlString: String, completion: @escaping(Data?, Error?) -> Void) {
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = URLSession.shared.dataTask(with: request) { (data, urlResponse, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+        task.resume()
+    } 
+}
